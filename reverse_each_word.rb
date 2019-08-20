261a68186a7653437ec13eb6d0b7b7126a78dfde
Andrew Thiessen <squaids449@gmail.com>
2019-08-20 22:09:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,14 @@ def reverse_each_word(sentence)
   i = 0
   new = []
   while i < sentence.length do
-    
+    yield(sentence[i])
+    i += 1
   end
   
 end
 
 
 reverse_each_word.each do |sentence|
-  sentence[i].reverse
+  sentence.reverse
   
 end

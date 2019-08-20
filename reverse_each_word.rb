8a002d17922deb4965a87c53211d3027c40c19de
Andrew Thiessen <squaids449@gmail.com>
2019-08-20 22:16:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-def reverse_each_word(sentence)
-  i = 0
-  new = []
-  while i < sentence.length do
-    new.push(yield(sentence[i]))
-    i += 1
-  end
-  new
-end
+# def reverse_each_word(sentence)
+#   i = 0
+#   new = []
+#   while i < sentence.length do
+#     new.push(yield(sentence[i]))
+#     i += 1
+#   end
+#   new
+# end
 
 
-reverse_each_word.each do |sentence|
-  sentence.reverse
+# reverse_each_word.each do |sentence|
+#   sentence.reverse
   
-end
+# end
